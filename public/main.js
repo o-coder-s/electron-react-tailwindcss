62eedbcde2ddaa19b87b5c5057b586dc65314017
= <=>
2021-04-12 01:09:35 +0100

    first commit

--- public/main.js
+++ public/main.js
@@ -10,6 +10,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true
